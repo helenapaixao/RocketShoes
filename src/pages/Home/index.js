3f0e2e0dfa6b54377414213d184c978c1155fe09
Helena Paixão <hp.helenapaixao@gmail.com>
2019-10-09 22:26:34 -0300

    config header and add logo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import {ProductList} from './styles'
+import {MdAddShoppingCart} from 'react-icons/md'
 
 export default function Home() {
-return <h1>Home</h1>
+return (
+  <ProductList>
+    <li>
+      <img src='https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_zoom2.jpg' alt="Tênis"/>
+      <strong>Tênis </strong>
+    <span>R$129,90</span>
+    <button type="button">
+      <div>
+        <MdAddShoppingCart size={16} color="#fff"/>
+      </div>
+      <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li>
+    <li>
+      <img src='https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_zoom2.jpg' alt="Tênis"/>
+      <strong>Tênis </strong>
+    <span>R$129,90</span>
+    <button type="button">
+      <div>
+        <MdAddShoppingCart size={16} color="#fff"/>
+      </div>
+      <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li>
+    <li>
+      <img src='https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_zoom2.jpg' alt="Tênis"/>
+      <strong>Tênis </strong>
+    <span>R$129,90</span>
+    <button type="button">
+      <div>
+        <MdAddShoppingCart size={16} color="#fff"/>
+      </div>
+      <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li>
+    <li>
+      <img src='https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_zoom2.jpg' alt="Tênis"/>
+      <strong>Tênis </strong>
+    <span>R$129,90</span>
+    <button type="button">
+      <div>
+        <MdAddShoppingCart size={16} color="#fff"/>
+      </div>
+      <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li>
+      <li>
+      <img src='https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_zoom2.jpg' alt="Tênis"/>
+      <strong>Tênis </strong>
+    <span>R$129,90</span>
+    <button type="button">
+      <div>
+        <MdAddShoppingCart size={16} color="#fff"/>
+      </div>
+      <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li>
+
+  </ProductList>
+)
 }
 
 
