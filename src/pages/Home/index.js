f1efdcb9d5de64fd67393dcd3fc2d6f3808c177e
Helena Paixão <hp.helenapaixao@gmail.com>
2019-10-11 17:05:13 -0300

    add count in button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -31,6 +31,7 @@ class Home extends Component {
   }
   render() {
     const { products } = this.state;
+    const {amount} = this.props;
     return (
       <ProductList>
         {products.map(product => (
@@ -40,7 +41,8 @@ class Home extends Component {
             <span>{formatPrice(product.price)}</span>
             <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
-                <MdAddShoppingCart size={16} color="#fff" />
+                <MdAddShoppingCart size={16} color="#fff" />{''}
+                {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -50,8 +52,14 @@ class Home extends Component {
     );
   }
 }
+const mapStateDispatchToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount ;
+    return amount;
+  }, {}),
+})
 
 const mapDispatchToProps = dispatch =>
 bindActionCreators(CartActions, dispatch)
 
-export default connect(null,mapDispatchToProps)(Home);
+export default connect(mapStateDispatchToProps ,mapDispatchToProps)(Home);
